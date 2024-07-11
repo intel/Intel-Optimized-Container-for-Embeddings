4fafc00800c64fe7756df43b9e1a5011d45aa615
Romir Desai <romir.desai@intel.com>
2024-07-11 15:02:38 -0700

    first release
    Signed-off-by: Romir Desai <romir.desai@intel.com>

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+eval ". ./miniconda3/bin/activate"
+
+eval "conda activate py310"
+
+serve_cmd="torchserve --start --ts-config=/home/ubuntu/config.properties --models ${MODEL_NAME_}=${MODEL_NAME_}.mar --model-store /home/ubuntu/model_store"
+
+eval $serve_cmd
+
+# prevent docker exit
+tail -f /dev/null
